d2f4fdea792a5c1eb5f824a5eb41ae7e63b58578
gmooreivy <gmoore138@ivytech.edu>
2025-09-03 09:33:27 -0400

    Update ifelseM02.py

--- ifelseM02.py
+++ ifelseM02.py
@@ -1,38 +1,43 @@
 # Gabriel Moore
 # ifelseM02.py
 # This program will accept students names (as a string) and GPA (as a float) and return if they can make the Dean's list or Honor Roll. If the entered first name is ZZZ, the program will terminate without any further processing
-
-lastName = str(input("Enter student last name: "))
-if lastName != "ZZZ" :
+while True :
+    lastName = str(input("Enter student last name: "))
+    if lastName == 'ZZZ' :
+        break
     firstName = str(input("Enter student first name: "))
     gpa = float(input("Enter student GPA as a decimal (e.g: 3.0): "))
 
-    if gpa > 3.5 :
-        print(firstName + " " + lastName + " Bis elegible for the Dean's List!")
-    elif gpa > 3.25 :
+    if gpa >= 3.5 :
+        print(firstName + " " + lastName + " is elegible for the Dean's List!")
+    elif gpa >= 3.25 :
         print(firstName + " " + lastName + " is elegible for the Honor Roll!")
     else :
-        print(firstName + " " + lastName + " is not elligble for any lists")
-        
+        print(firstName + " " + lastName + " is not elligble for any lists")   
         
 # //////////////////////////////////////////////////////////
 
 # TEST LOGS
 
-# Enter student last name: Moore
-# Enter student first name: Gabriel
-# Enter student GPA as a decimal (e.g: 3.0): 3.45
-# Gabriel Moore is elegible for the Honor Roll!
+"""
 
-# Enter student last name: ZZZ
-# (process ended)
+Enter student last name: Moore 
+Enter student first name: Gabriel
+Enter student GPA as a decimal (e.g: 3.0): 3.4
+Gabriel Moore  is elegible for the Honor Roll!
+Enter student last name: Baker
+Enter student first name: Michael
+Enter student GPA as a decimal (e.g: 3.0): 3.5
+Michael Baker is elegible for the Dean's List!
+Enter student last name: Privett
+Enter student first name: Aiden
+Enter student GPA as a decimal (e.g: 3.0): 3.0
+Aiden Privett is not elligble for any lists
+Enter student last name: Schorey
+Enter student first name: Paul
+Enter student GPA as a decimal (e.g: 3.0): 3.25
+Paul Schorey is elegible for the Honor Roll!
+Enter student last name: ZZZ
+(end of process)
 
-# Enter student last name: Baker
-# Enter student first name: Michael
-# Enter student GPA as a decimal (e.g: 3.0): 3.6
-# Michael Baker is elegible for the Dean's List!
-
-# Enter student last name: Hudson
-# Enter student first name: Cory
-# Enter student GPA as a decimal (e.g: 3.0): 2.1
-# Cory Hudson is not elligble for any lists
+"""
